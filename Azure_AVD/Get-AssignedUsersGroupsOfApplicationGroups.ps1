db1e416faef9b763a8b5b83372a289d48a08e3fb
[Andreas Baumgarten] <abaumgarten@outlook.com>
2023-09-30 07:25:03 +0200

    Update

--- Azure_AVD/Get-AssignedUsersGroupsOfApplicationGroups.ps1
+++ Azure_AVD/Get-AssignedUsersGroupsOfApplicationGroups.ps1
@@ -4,7 +4,7 @@ $appGroupName = Get-AzWvdApplicationGroup | Select-Object Name
 $appGroupName | ForEach-Object {
     $name = $_.Name
     # Get all assignments
-    $assigments = Get-AzRoleAssignment | Where-Object { $_.Scope -like "*/$name" }
+    $assignments = Get-AzRoleAssignment | Where-Object { $_.Scope -like "*/$name" }
     # Prepare output (Displayname, ObjectType,ActionGgroup)
-    $assigments | Select-Object DisplayName, ObjectType, @{N = 'ActionGroup'; E = { $_.Scope.Split('/')[-1] } }
+    $assignments | Select-Object DisplayName, ObjectType, @{N = 'ActionGroup'; E = { $_.Scope.Split('/')[-1] } }
 }
